28624f14991f79cff89a2cf3d696e430f3863461
lmmmowi <lmmmowi@163.com>
2019-05-15 16:36:34 +0800

    修复README解析Bug

--- src/main/java/com/lmmmowi/leetcode/ReadMeGenerator.java
+++ src/main/java/com/lmmmowi/leetcode/ReadMeGenerator.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
  */
 public class ReadMeGenerator {
 
-    private static final Pattern pattern = Pattern.compile(".*(\\d+)\\.(.*)\\[(.*)\\]");
+    private static final Pattern pattern = Pattern.compile(".* (\\d+)\\.(.*)\\[(.*)\\]");
 
     public static void main(String[] args) {
         new ReadMeGenerator().run();
