4ef9135ea57c884406656d0d15187d6498bc68ee
lmmmowi <lmmmowi@163.com>
2021-04-06 09:56:12 +0800

    update

--- src/main/java/com/lmmmowi/leetcode/p1171/Solution.java
+++ src/main/java/com/lmmmowi/leetcode/p1171/Solution.java
@@ -1,8 +1,5 @@
 package com.lmmmowi.leetcode.p1171;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * @Author: lmmmowi
  * @Date: 2021/3/31
@@ -11,53 +8,20 @@ import java.util.List;
 public class Solution {
 
     public ListNode removeZeroSumSublists(ListNode head) {
-        List<Record> records = new ArrayList<>();
-
-        ListNode node = head, prev = null;
+        int sum = 0;
+        ListNode node = head;
         while (node != null) {
-            records.add(new Record(node, prev));
-
-            int targetIndex = -1;
-            for (int i = records.size() - 1; i >= 0; i--) {
-                Record record = records.get(i);
-                int sum = record.sum + node.val;
-                if (sum == 0) {
-                    targetIndex = i;
-                } else {
-                    record.sum = sum;
-                }
-            }
-
-            if (targetIndex >= 0) {
-                Record targetRecord = records.get(targetIndex);
-                for (int i = records.size() - 1; i >= targetIndex; i--) {
-                    records.remove(i);
-                }
-
-                prev = targetRecord.prev;
-                if (prev == null) {
-                    head = node.next;
-                } else {
-                    prev.next = node.next;
-                }
-            } else {
-                prev = node;
-            }
-
+            sum += node.val;
             node = node.next;
+
+            if (sum == 0) {
+                head = node;
+            }
+        }
+
+        if (head != null) {
+            head.next = removeZeroSumSublists(head.next);
         }
         return head;
-    }
-
-
-    private class Record {
-        ListNode node;
-        ListNode prev;
-        int sum;
-
-        private Record(ListNode node, ListNode prev) {
-            this.node = node;
-            this.prev = prev;
-        }
     }
 }
