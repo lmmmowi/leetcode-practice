7f3f7bdbb72a95a8aa4016235022734019896fd2
lmmmowi <lmmmowi@163.com>
2021-05-11 14:11:25 +0800

    update

--- src/main/java/com/lmmmowi/leetcode/p1559/Solution.java
+++ src/main/java/com/lmmmowi/leetcode/p1559/Solution.java
@@ -7,13 +7,18 @@ package com.lmmmowi.leetcode.p1559;
  */
 public class Solution {
 
+    private static final int[][] STEPS = new int[][]{
+            new int[]{0, -1}, new int[]{0, 1},
+            new int[]{-1, 0}, new int[]{1, 0}
+    };
+
     public boolean containsCycle(char[][] grid) {
         boolean[][] visited = new boolean[grid.length][grid[0].length];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 char c = grid[i][j];
                 if (c > 0) {
-                    boolean found = search(grid, c, i, j, 0, 0, -1, -1, visited);
+                    boolean found = search(grid, c, i, j, 0, 0, visited);
                     if (found) {
                         return true;
                     }
@@ -24,34 +29,39 @@ public class Solution {
         return false;
     }
 
-    public boolean search(char[][] grid, char c, int x, int y, int fx, int fy, int nx, int ny, boolean[][] visited) {
-        // 不允许倒退
-        if (x == nx && y == ny) {
-            return false;
-        }
-
-        // 不允许越界
-        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length) {
-            return false;
-        }
-
-        // 检测到环
-        if (visited[x][y]) {
-            return true;
-        }
-
-        // 字母必须相同
-        if (grid[x][y] != c) {
-            return false;
-        }
-
+    public boolean search(char[][] grid, char c, int x, int y, int fx, int fy, boolean[][] visited) {
         visited[x][y] = true;
-        boolean found = search(grid, c, x, y - 1, x, y, fx, fy, visited);
-        found = found || search(grid, c, x, y + 1, x, y, fx, fy, visited);
-        found = found || search(grid, c, x - 1, y, x, y, fx, fy, visited);
-        found = found || search(grid, c, x + 1, y, x, y, fx, fy, visited);
+
+        for (int[] step : STEPS) {
+            int nx = x + step[0], ny = y + step[1];
+
+            // 不允许倒退
+            if (nx == fx && ny == fy) {
+                continue;
+            }
+
+            // 不允许越界
+            if (nx < 0 || nx >= grid.length || ny < 0 || ny >= grid[0].length) {
+                continue;
+            }
+
+            // 字母必须相同
+            if (grid[nx][ny] != c) {
+                continue;
+            }
+
+            // 检测到环
+            if (visited[nx][ny]) {
+                return true;
+            }
+
+            if (search(grid, c, nx, ny, x, y, visited)) {
+                return true;
+            }
+        }
+
         visited[x][y] = false;
         grid[x][y] = 0;
-        return found;
+        return false;
     }
 }
